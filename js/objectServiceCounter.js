c005c45abb98af3dc152bd3f912b80348b528acb
Ilya <sijonra@gmail.com>
2021-11-16 18:28:28 +0300

    counter script fixed

---
+++ js/objectServiceCounter.js
@@ -0,0 +1,70 @@
+function outNum1(num, elem) {
+    let time = 3000;
+    let step = 500; 
+    let f = document.querySelector('#out1');
+    a = 0;
+    let s = Math.round(time/(num/step));
+    let interval = setInterval(() => {
+        a = a + step;
+        if(a == num) {
+            clearInterval(interval);
+        }
+        f.innerHTML = a;
+    },
+  s);
+};
+outNum1(50000, '#out1');  
+
+
+function outNum2(num, elem) {
+    let time = 3000;
+    let step = 500; 
+    let q = document.querySelector('#out2');
+    m = 0;
+    let z = Math.round(time/(num/step));
+    let interval = setInterval(() => {
+        m = m + step;
+        if(m == num) {
+            clearInterval(interval);
+        }
+        q.innerHTML = m;
+    },
+    z);
+};
+outNum2(42000, '#out2');  
+
+
+function outNum3(num, elem) {
+    let time = 3000;
+    let step = 200; 
+    let n = document.querySelector('#out3');
+    p = 0;
+    let w = Math.round(time/(num/step));
+    let interval = setInterval(() => {
+        p = p + step;
+        if(p == num) {
+            clearInterval(interval);
+        }
+        n.innerHTML = p;
+    },
+  w);
+};
+outNum3(25000, '#out3');
+
+
+function outNum4(num, elem) {
+    let time = 3000;
+    let step = 200; 
+    let i = document.querySelector('#out4');
+    d = 0;
+    let b = Math.round(time/(num/step));
+    let interval = setInterval(() => {
+        d = d + step;
+        if(d == num) {
+            clearInterval(interval);
+        }
+        i.innerHTML = d;
+    },
+  b);
+};
+outNum4(18000, '#out4');  
