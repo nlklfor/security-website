cc9f7513d6b64b712d35bea99dd19bc7edd303d7
Ilya <sijonra@gmail.com>
2021-11-17 23:14:58 +0300

    added section services-slider

--- js/script.js
+++ js/script.js
@@ -15,4 +15,36 @@ burgerMenu();
 
 // === /Burger ===
 
+// === Swiper ===
 
+const swiper = new Swiper('.swiper', {
+  
+    slidesPerView: 3,
+    spaceBetween: 40,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next-unique',
+        prevEl: '.swiper-button-prev-unique'
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+
+    breakpoints:{
+        1000:{
+            slidesPerView: 3,
+        },
+        600:{
+            slidesPerView: 2,
+        },
+        300:{
+            slidesPerView: 1,
+        },
+    }
+
+});
+
+// === /Swiper ===
